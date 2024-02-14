255103376bba92ff7391d6e010fa2169877b92d2
pedram-macbook <pedram@email.com>
2024-02-14 17:48:44 +0330

    add user admin tests and show users in admin panel

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
+"""
+Django Admin customization
+"""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """define the admin page for users"""
+    ordering = ('id',)
+    list_display = ('email', 'name')
+
+
+admin.site.register(models.User, UserAdmin)
