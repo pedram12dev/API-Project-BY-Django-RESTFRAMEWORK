f12d54152c10cd1a4b37c7e0b847dde0c1dbfb9d
pedram-macbook <pedram@email.com>
2024-02-17 19:12:01 +0330

    add serializer for recipe api

---
+++ app/recipe/serializers.py
@@ -0,0 +1,13 @@
+"""serializers for recipe api"""
+
+from rest_framework import serializers
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """serializer for recipe"""
+
+    class Meta:
+        model = Recipe
+        fields = ('id','title','time_minutes','price','link')
+        read_only_fields = ('id',)
