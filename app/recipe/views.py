40d92b0be5fd6f2cee4191fd49d636e310cd2624
pedram-macbook <pedram@email.com>
2024-02-17 19:13:12 +0330

    add view for recipe api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+""" views for the recipe apis"""
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """view for manage recipe api"""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """retrieve the recipes for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
