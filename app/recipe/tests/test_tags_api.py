65fd3d4168ad9aaffed782994737c080c7aa2dc3
pedram-macbook <pedram@email.com>
2024-02-19 11:17:46 +0330

    add test deleting tag

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -73,3 +73,12 @@ class PrivateTagsApiTest(TestCase):
         self.assertEqual(res.status_code , status.HTTP_200_OK)
         tag.refresh_from_db()
         self.assertEqual(tag.name , payload['name'])
+
+    def test_delete_tag(self):
+        """ test deleting tag"""
+        tag = Tag.objects.create(user=self.user, name='mahan')
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter().exists()
+        self.assertFalse(tags.exists())
